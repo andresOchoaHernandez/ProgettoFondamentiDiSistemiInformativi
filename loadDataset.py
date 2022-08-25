9ee97ae312d5583eab1f49a03f867315bb97b0ea
andresOchoaHernandez <52919052+andresOchoaHernandez@users.noreply.github.com>
2022-08-25 11:33:40 +0200

    Add files via upload

--- loadDataset.py
+++ loadDataset.py
@@ -1,5 +1,6 @@
 import boto3
 from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.conditions import Attr
 
 import csv
 import glob
@@ -74,9 +75,10 @@ def populate_tables(dynamoDbClient):
     dispositiviTable = dynamoDbClient.Table('Dispositivi')
 
     listOfCards = {}
+    listOfIngressi = []
     listOfDispositivi = {}
 
-    limit = 0
+    limit = 100
 
     for filename in glob.glob('./dataset/*.csv'):
         
@@ -88,6 +90,9 @@ def populate_tables(dynamoDbClient):
                 ingresso = {}
                 dispositivo = {}
 
+                """
+                Inserimento VeronaCard
+                """
                 if row[4] not in listOfCards.keys() and len(listOfCards) <= 24: 
                     veronaCard['CodiceSeriale'] = row[4]
                     veronaCard['ChiaveOrdinamento'] = 'Info'
@@ -102,31 +107,49 @@ def populate_tables(dynamoDbClient):
                             batch.put_item(Item=listOfCards[item])
                     listOfCards.clear()
                 
+                """
+                Inserimento Ingresso
+                """
+                if len(listOfIngressi) <= 24 : 
+                    ingresso['CodiceSeriale'] = row[4]
+                    ingresso['ChiaveOrdinamento'] = row[3] + "_" + row[0] +"_" + row[1]
 
-                ingresso['CodiceSeriale'] = row[4]
-                ingresso['ChiaveOrdinamento'] = row[3]
-                ingresso['Data'] = row[0]
-                ingresso['Ora'] = row[1]
+                    listOfIngressi.append(ingresso)
 
-                veronaCardsTable.put_item(Item = ingresso)
+                if len(listOfIngressi) == 25:
+                    with veronaCardsTable.batch_writer() as batch:
+                        for item in listOfIngressi:
+                            veronaCardsTable.put_item(Item = item)
+                    listOfIngressi.clear()
 
-                if row[3] not in listOfDispositivi.keys() and len(listOfDispositivi) <= 24:
+                """
+                Inserimento Dispositivo
+                """
+                if row[3] not in listOfDispositivi.keys() and len(listOfDispositivi) <= 4:
                     dispositivo['Codice'] = row[3]
                     dispositivo['Name'] = row[2]
 
                     listOfDispositivi[row[3]] = dispositivo
                 
-                if len(listOfDispositivi) == 25:
+                if len(listOfDispositivi) == 5:
                     with dispositiviTable.batch_writer() as batch:
-                        for item in listOfDispositivi:
-                            batch.put_item(Item=item)
+                        for item in listOfDispositivi.keys():
+                            batch.put_item(Item=listOfDispositivi[item])
+                    listOfDispositivi.clear()
 
+                """
+                Limite per il testing
+                """
                 if limit == LIMIT:
                     limit = 0
                     break
 
                 limit+=1
 
+def scan_table(dynamoDbClient,table_name):
+    response = dynamoDbClient.Table(table_name).scan()
+    for item in response['Items']:
+        print(item)
 
 def main():
 
@@ -138,18 +161,43 @@ def main():
         aws_secret_access_key= 'password'
     )
 
-    #create_tables(dynamoDbClient)
-    #populate_tables(dynamoDbClient)
+    create_tables(dynamoDbClient)
+    populate_tables(dynamoDbClient)
 
-    response = dynamoDbClient.Table('VeronaCards').get_item(TableName='VeronaCards',Key={'CodiceSeriale':'049B31523F3880','ChiaveOrdinamento':'Info'})
+    scan_table(dynamoDbClient,'VeronaCards')
 
+    """
+    Ottenere una certa veronacard
+    """
+    response = dynamoDbClient.Table('VeronaCards').get_item(TableName='VeronaCards',Key={'CodiceSeriale':'04FA80523F3880','ChiaveOrdinamento':'Info'})
     print(response['Item'])
 
-    response = dynamoDbClient.Table('VeronaCards').query(TableName='VeronaCards',KeyConditionExpression = Key('CodiceSeriale').eq('049B31523F3880'))
+    print("================================================================")
 
+    """
+    Ingressi di una certa veronacard con un certo dispositivo
+    """
+    response = dynamoDbClient.Table('VeronaCards').query(TableName='VeronaCards',KeyConditionExpression= Key('CodiceSeriale').eq('04FA80523F3880')&Key('ChiaveOrdinamento').begins_with('40'))
     for item in response['Items']:
         print(item)
 
+    print("================================================================")
+    
+    """
+    Ingressi in un certo dispositivo
+    """
+    response = dynamoDbClient.Table('VeronaCards').scan(FilterExpression=Attr('ChiaveOrdinamento').begins_with('40'))
+    for item in response['Items']:
+        print(item)
+
+    print("================================================================")
+
+    """
+    Ingressi in una certa data
+    """
+    response = dynamoDbClient.Table('VeronaCards').scan(FilterExpression=Attr('ChiaveOrdinamento').contains('30-12-16'))
+    for item in response['Items']:
+        print(item)
 
 if __name__ == '__main__':
     main()
